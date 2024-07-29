b591f5afb0718ee922efa7d041bc4273a3749ee8
OmTasg <137707066+OmTasg@users.noreply.github.com>
2024-07-29 17:18:35 -0400

    fixed small bugs

--- scripts.js
+++ scripts.js
@@ -13,33 +13,33 @@ const highLow = document.getElementById("highlow");
 const humidityDisplay = document.getElementById("humidity");
 const windDisplay = document.getElementById("wind");
 const unitInput = document.getElementById("unit-select");
+const container = document.getElementById("container");
 let unitType = "";
+const mainContent = document.getElementById("main-content");
 
 async function checkWeather() {
   if (cityInput.value != "") {
-    const response = await fetch(
-      `${apiUrl}q=${city}&appid=${apiKey}&units=${unitInput.value}`
-    );
+    const response = await fetch(`${apiUrl}q=${city}&appid=${apiKey}&units=${unitInput.value}`);
     const data = await response.json();
     if (response.status == 400 || response.status == 404) {
       errorMsg.innerText = "Invalid City Name";
+      deleteWeather()
+      
     } else {
       errorMsg.innerText = "";
       picDiv.innerHTML = `<img src = ${`https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}>`;
       cityTitle.innerText = data.name;
       tempDisplay.innerText = `${Math.round(data.main.temp)}°`;
       desc.innerText = data.weather[0].description;
-      highLow.innerText = `H: ${Math.round(
-        data.main.temp_max
-      )}° | L: ${Math.round(data.main.temp_min)}°`;
+      highLow.innerText = `H: ${Math.round(data.main.temp_max)}° | L: ${Math.round(data.main.temp_min)}°`;
       humidityDisplay.innerText = `Humidity: ${data.main.humidity}%`;
       windDisplay.innerText = `Wind: ${Math.round(data.wind.speed)}`;
     }
 
-    if (unitInput.value === "imperial") {
+    if (unitInput.value === "imperial" && !(response.status == 400 || response.status == 404)) {
       tempDisplay.innerText += "F";
       windDisplay.innerText += " MPH";
-    } else {
+    } else if (unitInput.value === "metric" && !(response.status == 400 || response.status == 404)) {
       tempDisplay.innerText += "C";
       windDisplay.innerText += " KPH";
     }
@@ -56,3 +56,14 @@ cityInput.addEventListener("keypress", function (event) {
     checkBtn.click();
   }
 });
+
+
+function deleteWeather(){
+  picDiv.innerHTML = ""
+  cityTitle.innerText = ""
+  tempDisplay.innerText = ""
+  desc.innerText = ""
+  highLow.innerText = ""
+  humidityDisplay.innerText = ""
+  windDisplay.innerText = ""
+}
